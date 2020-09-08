8789260606982209eadba3429a0c45ae744673d7
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-09-08 10:00:57 +0200

    Uppgiften är klar!

---
+++ MyFirstServer/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const clientDir = __dirname + "\\client\\"
+const clientImages = __dirname + "\\client\\images\\"
+
+app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
+app.get('/generalStyling', (req, res) => res.sendFile(clientDir + "generalStyling.CSS"))
+
+app.get('/MoreProductionFavicon', (req, res) => res.sendFile(clientImages + "MoreProductionFavicon.jpg"))
+app.get('/Longedrag', (req, res) => res.sendFile(clientImages + "Longedrag.jpg"))
+app.get('/DroneCarOnBridge', (req, res) => res.sendFile(clientImages + "DroneCarOnBridge.jpg"))
+app.get('/DroneBridge', (req, res) => res.sendFile(clientImages + "DroneBridge.jpg"))
+app.get('/Hinsholmen', (req, res) => res.sendFile(clientImages + "Hinsholmen.jpg"))
+app.get('/TentSky', (req, res) => res.sendFile(clientImages + "TentSky.jpg"))
+app.get('/Mojje', (req, res) => res.sendFile(clientImages + "Mojje.jpg"))
+app.get('/MojjeLandingsPage', (req, res) => res.sendFile(clientImages + "MojjeLandingsPage.png"))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
