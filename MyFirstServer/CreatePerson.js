f3f0dbe9c80553699ae7084b7be0f264bf5b0397
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-10-06 10:48:28 +0200

    Nu är den klart!!!

---
+++ MyFirstServer/CreatePerson.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+
+const Element = mongoose.model('Element', personSchema);
+
+exports.createPerson = (name, email) =>{
+    var newElement = new Element({
+        name: name,
+        email: email
+    }) 
+    return newElement
+}
--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 // For connect the app.js to the database.Line 6-12
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/formInformations', {useNewUrlParser: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
-});
+const dbModule = require('./dbModules')
 
 const clientDir = __dirname + "\\client\\"
 const clientImages = __dirname + "\\client\\images\\"
 
+const personModule = require('./CreatePerson')
+
 var bodyParser = require('body-parser')
+const { Schema } = require('mongoose')
 
 app.use(bodyParser())
 
@@ -32,25 +28,13 @@ app.get('/TentSky', (req, res) => res.sendFile(clientImages + "TentSky.jpg"))
 app.get('/Mojje', (req, res) => res.sendFile(clientImages + "Mojje.jpg"))
 app.get('/MojjeLandingsPage', (req, res) => res.sendFile(clientImages + "MojjeLandingsPage.png"))
 
-
-const peopleSchema = new mongoose.Schema({
-    name : String,
-    email : String
-})
-
-const People = mongoose.model('People',peopleSchema)
-
 app.get('/', (req, resp) => {
     resp.sendFile('index.html', {root: path.join(clientDir)})
 })
 app.post('/', (req, resp) => {
-    var name = req.body.name
-    var email = req.body.email
+    let person = personModule.createPerson(req.body.name, req.body.email)
 
-    const peopleInformation = new People({name, email})
-
-    peopleInformation.save()
-
+    dbModule.store(person)
     resp.redirect('/')
 })
 
