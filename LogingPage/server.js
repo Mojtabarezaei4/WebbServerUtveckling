e2684c84b9bf7e3a0bd8ad303071733887b0d497
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-10-13 22:15:30 +0200

    It's working now. Exept the connection to the db

---
+++ LogingPage/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Connecting to the data base
+const dbModule = require('./DataBase/dbModules')
+const personModule = require('./DataBase/CreatePerson')
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.post('/', (req, res) =>{
+    let person = personModule.createPerson(req.body.name, req.body.email)
+
+    dbModule.store(person)
+    resp.render()
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
