bbf937f9cf402266348626e8079c666cc7b2a2f4
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2021-03-22 17:29:52 +0100

    Authontication app is done!✨

--- firebase-react-authentication/src/components/Login.js
+++ firebase-react-authentication/src/components/Login.js
@@ -1,13 +1,12 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
-import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from "react-router-dom"
+import { useAuth } from '../contexts/AuthContext'
 
-export default function Signup() {
+export default function Login() {
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { signup } = useAuth()
+    const { login } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const history = useHistory()
@@ -15,21 +14,17 @@ export default function Signup() {
     async function handleSubmit(e) {
         e.preventDefault()
 
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError("Passwords do not match")
-        }
-
         try{
             setError("")
             setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value)
-            history.push("/login")
+            await login(emailRef.current.value, passwordRef.current.value)
+            history.push("/")
         } catch {
             if ((passwordRef.current.value === '' || passwordRef.current.value.indexOf(6))){
-                setError("Password needs to be at least 6 characters!")
+                setError("Wrong password!")
             }
             else{
-                setError("Fail to create an account!")
+                setError("Fail to sign in!")
             }
         }
         setLoading(false)
@@ -38,7 +33,7 @@ export default function Signup() {
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
+                    <h2 className="text-center mb-4">Log In</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
@@ -49,16 +44,15 @@ export default function Signup() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
+                        <Button disabled={loading} className="w-100" type="submit">Log In</Button>
                     </Form>
+                    <div className="w-100 text-center mt-3">
+                        <Link to="/forgot-password">Forgot Password?</Link>
+                    </div> 
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? <Link to="/login">Log In</Link>
+                Need an account? <Link to="/signup">Sign Up</Link>
             </div>   
         </>
     )
