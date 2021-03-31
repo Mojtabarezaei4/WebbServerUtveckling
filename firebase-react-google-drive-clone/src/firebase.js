0fe8c204da9586b04f465585f2e036b47c72cfaf
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2021-03-31 20:01:28 +0200

    Added firebase, dashboard and navbar

---
+++ firebase-react-google-drive-clone/src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBAS_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBAS_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBAS_MASSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBAS_APP_ID
+})
+
+const firestore = app.firestore()
+export const database = {
+    folders: firestore.collection('folders'),
+    files: firestore.collection('files'),
+    getCurrentTimestamp: firebase.firestore.FieldValue.serverTimestamp
+
+}
+export const auth = app.auth()
+export default app
